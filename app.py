a8989ac8f61c16f4c63c53181cc3e27eb61a945d
sasdev95 <108204575+sasdev95@users.noreply.github.com>
2023-02-18 06:46:19 -1000

    Change folder upload location to AWS S3

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ from email import encoders
 import random
 
 # Declare path and restrictions for recipe file uploads
-UPLOAD_FOLDER = '/static/documents'
+UPLOAD_FOLDER = 's3://recipe-swap/documents/'
 ALLOWED_EXTENSIONS = {'txt', 'doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'jfif', 'gif', 'mhtml'}
 
 # Declare path for email text file
 EMAIL_FOLDER = '/templates'
 
 # Declare path and restrictions for food photo uploads
-PHOTO_UPLOAD_FOLDER = '/static/photos'
+PHOTO_UPLOAD_FOLDER = 's3://recipe-swap/photos/'
 PHOTO_ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'jfif', 'gif'}
 
 # Configure application
@@ -173,7 +173,7 @@ def swap():
             submitted_files[i] = re.sub(r"[\(\)]", '', submitted_files[i])
 
         # Get list of all files stored in the documents folder, in no particular order
-        documents = os.listdir('/static/documents')
+        documents = os.listdir('s3://recipe-swap/documents/')
 
         # Initialize list for recipes to be assigned to participants
         recipes = []
@@ -220,7 +220,7 @@ def swap():
             # Used sequence for attaching file from "https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/"
 
             # Read recipe file to be sent
-            file = open(f'/static/documents/{filename}', "rb")
+            file = open(f's3://recipe-swap/documents/{filename}', "rb")
 
             # Instance of MIMEBase, named as 'attachment'
             attachment = MIMEBase('application', 'octet-stream')
